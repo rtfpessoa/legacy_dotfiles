6028b814170a7a30480b59f494ce6330e3a54db4
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2016-08-24 20:19:51 +0100

    Fix nvm in some shells

--- shells/sources/rtfpessoa.sh
+++ shells/sources/rtfpessoa.sh
@@ -156,10 +156,9 @@ fi
 
 export NVM_DIR="$HOME/.nvm"
 
-declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
 NODE_GLOBALS+=("node")
 NODE_GLOBALS+=("nvm")
-NODE_GLOBALS+=("npm")
 
 load_nvm () {
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
