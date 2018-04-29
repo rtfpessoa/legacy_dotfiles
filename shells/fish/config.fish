8aa696fed71b1d284c10767944542955209689a2
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2018-04-29 18:37:36 +0100

    Customize theme parameters

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -1,5 +1,12 @@
 # Fish
 
-set --erase fish_greeting
-# set -gx fish_greeting ""
-# set -gx fish_greeting '(fortune -a)'
+function fish_greeting; end
+# function fish_greeting
+#   set_color $fish_color_autosuggestion
+#   fortune -a
+#   set_color normal
+# end
+# function fish_right_prompt; end
+
+# set -g default_user your_normal_user
+set -g fish_prompt_pwd_dir_length 1
